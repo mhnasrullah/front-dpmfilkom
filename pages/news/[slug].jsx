7a30e66d35ba8e98d78102137c16fad53d5206ab
Hasan Nasrullah <hnasrul22@gmail.com>
2022-09-26 11:14:05 +0700

    update .

--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -15,7 +15,7 @@ import "swiper/css"
 import "swiper/css/navigation"
 import Footer from '../../section/Footer';
 
-export default function detailNews() {
+export default function DetailNews() {
     const router = useRouter();
     const {slug} = router.query;
     const [data,setData] = useState({});
